17cf85d526ef1ce8eb92e5dc3e5b8077019d5a40
Kuba Karpierz <mr.karpierz@gmail.com>
2025-08-03 11:50:24 -0400

    list all rooms

--- slax/lib/slax_web/live/chat_room_live.ex
+++ slax/lib/slax_web/live/chat_room_live.ex
@@ -6,6 +6,35 @@ defmodule SlaxWeb.ChatRoomLive do
 
   def render(assigns) do
     ~H"""
+    <div class="flex flex-col shrink-0 w-64 bg-slate-100">
+      <div class="flex justify-between items-center shrink-0 h-16 border-b border-slate-300 px-4">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-lg font-bold text-gray-800">
+            Slax
+          </h1>
+        </div>
+      </div>
+      <div class="mt-4 overflow-auto">
+        <div class="flex items-center h-8 px-3">
+          <span class="ml-2 leading-none font-medium text-sm">Rooms</span>
+        </div>
+        <div id="rooms-list">
+          <!-- Syntax which indicates looping using function component-->
+          <!-- :for is shorthand syntax for
+          <%= for room <- @rooms do %>
+            <.room_link room={room} active={room.id == @room.id} />
+          <% end %>
+          and
+
+          <.room_link room={room} active={room.id == @room.id} />
+
+          is shorthand for
+
+          {room_link(%{room: room, active: room.id == @room.id})}
+          --> <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+        </div>
+      </div>
+    </div>
     <div>Welcome to the chat!</div>
     <div class="flex flex-col grow shadow-lg">
       <div class="flex justify-between items-center shrink-0 h-16 bg-white border-b border-slate-300 px-4">
@@ -29,6 +58,38 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
+  ## Define the "assigns" passed to room_link
+  ##
+  ## Not required, but allows compiler to warn if wrong type is passed.
+  ## Can take :default
+  ##
+  attr :active, :boolean, required: true
+  attr :room, Room, required: true
+
+  ## Function component - takes an assigns map as arg, returns ~H
+  ## https://hexdocs.pm/phoenix_live_view/Phoenix.Component.html
+  defp room_link(assigns) do
+    ~H"""
+    <a
+      class={
+        [
+          "flex items-center h-8 text-sm pl-8 pr-3",
+          if(@active, do: "bg-slate-300", else: "hover:bg-slate-300")
+          ## equivelant
+          ## (@active && "bg-slate-300") || "hover:bg-slate-300"
+          ## if(@active, do: "bg-slate-300", else: "hover:bg-slate-300")
+        ]
+      }
+      href="#"
+    >
+      <.icon name="hero-hashtag" class="h-4 w-4" />
+      <span class={["ml-2 leading-none", @active && "font-bold"]}>
+        {@room.name}
+      </span>
+    </a>
+    """
+  end
+
   def mount(_params, _session, socket) do
     if connected?(socket) do
       IO.puts("mounting (connected)")
@@ -36,11 +97,13 @@ defmodule SlaxWeb.ChatRoomLive do
       IO.puts("mounting (not connected)")
     end
 
-    room = Room |> Repo.all() |> List.first()
+    rooms = Repo.all(Room)
+    room = rooms |> List.first()
 
     socket =
       socket
       |> assign(:room, room)
+      |> assign(:rooms, rooms)
       |> assign(:hide_topic?, false)
 
     {:ok, socket}
