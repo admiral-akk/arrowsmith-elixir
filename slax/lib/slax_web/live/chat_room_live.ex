636097646f6547a19072c74b711ae6bcee554c5a
Kuba Karpierz <mr.karpierz@gmail.com>
2025-08-03 14:19:10 -0400

    add login

--- slax/lib/slax_web/live/chat_room_live.ex
+++ slax/lib/slax_web/live/chat_room_live.ex
@@ -59,6 +59,28 @@ defmodule SlaxWeb.ChatRoomLive do
             <% end %>
           </div>
         </div>
+        <ul class="relative z-10 flex items-center gap-4 px-4 sm:px-6 lg:px-8 justify-end">
+          <li class="text-[0.8125rem] leading-6 text-zinc-900">
+            {@current_user.email}
+          </li>
+          <li>
+            <.link
+              href={~p"/users/settings"}
+              class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+            >
+              Settings
+            </.link>
+          </li>
+          <li>
+            <.link
+              href={~p"/users/log_out"}
+              method="delete"
+              class="text-[0.8125rem] leading-6 text-zinc-900 font-semibold hover:text-zinc-700"
+            >
+              Log out
+            </.link>
+          </li>
+        </ul>
       </div>
     </div>
     """
@@ -141,6 +163,7 @@ defmodule SlaxWeb.ChatRoomLive do
     {:noreply, socket}
   end
 
+  @spec handle_event(<<_::96>>, any(), map()) :: {:noreply, map()}
   def handle_event("toggle-topic", _params, socket) do
     socket =
       socket
